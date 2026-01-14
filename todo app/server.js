35a82350afb0e6fb1965f8f21dd14859f601699b
godlisten-ngatara <gpn0021@gmail.com>
2026-01-14 12:31:48 +0300

    Port forward to access the application locally

--- todo app/server.js
+++ todo app/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Todo App!');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
