31afc7227765f4b0e632d52f5ffad175c31bfa7c
godlisten-ngatara <gpn0021@gmail.com>
2026-01-20 02:18:40 +0300

    feat: Update Dockerfile and app.js for Express integration; add Ingress and Service manifests

--- log output/app.js
+++ log output/app.js
@@ -1,13 +1,19 @@
+const express = require("express");
 const crypto = require("crypto");
 
-// Output a random string every 5 seconds with a timestamp using a loop
-async function logLoop() {
-  while (true) {
-    const randomString = crypto.randomUUID();
-    const timestamp = new Date().toISOString();
-    console.log(`${timestamp}: ${randomString}`);
-    await new Promise(resolve => setTimeout(resolve, 5000));
-  }
-}
+const app = express();
+const port = process.env.PORT || 3000;
 
-logLoop();
+app.get("/current-status", (req, res) => {
+  const randomString = crypto.randomUUID();
+  const timestamp = new Date().toISOString();
+
+  res.json({
+    timestamp,
+    randomString,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
